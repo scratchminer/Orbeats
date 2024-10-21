18e4f6551af4067a3c884610a22af346725c4419
scratchminer <scratchminer@gmail.com>
2024-10-21 14:08:12 -0400

    add map creation to editor/maps.lua

--- Source/editor/maps.lua
+++ Source/editor/maps.lua
@@ -2,6 +2,7 @@
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
+local kb <const> = pd.keyboard
 
 local screenWidth <const> = 400
 local screenHeight <const> = 240
@@ -27,27 +28,47 @@ local mapSelection = 1
 local mapSelectionRounded = mapSelection
 local oldMapSelection = mapSelection
 
+local oldText = ""
+
 local mapHeaderX, mapHeaderY = 3, 3
 
 local currentSongData = {}
 
 local function getListOfMaps()
-	local songFiles = pd.file.listFiles("/editor songs/" .. makeValidFilename(currentSongData.name) .. "." .. makeValidFilename(currentSongData.artist))
-	if songFiles == nil then songFiles = {} end
+	local maps = {}
 	
-	for i = #songFiles, 1, -1 do
-		if songFiles[i]:sub(-5) ~= '.json' or songFiles[i] == "songData.json" then
-			table.remove(songFiles, i)
-		else
-			songFiles[i] = songFiles[i]:sub(1, -6)
-		end
+	for name, _ in pairs(currentSongData.mapData) do
+		maps[#maps + 1] = name
 	end
 	
-	return songFiles
+	return maps
+end
+
+local function keyboardClosing(confirmed)
+	if confirmed then
+		if selecting == "map" then
+			currentSongData.mapData[kb.text] = {notes = {}, effects = {}, songEnd = 0}
+		elseif selecting == "options" then
+			currentSongData.mapData[kb.text] = currentSongData.mapData[oldText]
+			currentSongData.mapData[oldText] = nil
+		end
+		setMapOptionsData(currentSongData)
+	else
+		songMaps[mapSelectionRounded] = oldText
+		setMapOptionsData(currentSongData)
+	end
+	
+	sfx.switch:play()
+end
+
+local function keyboardChanged()
+	if selecting == "map" then
+		songMaps[mapSelectionRounded] = kb.text
+	end
 end
 
 -- this function sets the song that the draw and update functions will pull from when getting maps.
--- this takes in the songData.json data
+-- this takes in the song's editor data
 function setMapOptionsData(data)
 	currentSongData = data
 	
@@ -56,38 +77,45 @@ function setMapOptionsData(data)
 end
 
 function updateMapSongsSelect()
-	if selecting == "option" then
-		if aPressed or rightPressed then
-			sfx.switch:play()
-			-- do stuff here
-		elseif bPressed or leftPressed then
-			sfx.switch:play()
-			selecting = "map"
-		elseif upPressed then
-			optionSelection -= 1
-		elseif downPressed then
-			optionSelection += 1
-		end
-		
-		optionSelection += crankChange/90
-	elseif selecting == "map" then
-		if aPressed or rightPressed then
-			sfx.switch:play()
-			if mapSelectionRounded == #songMaps then
-				-- pull up keyboard to create map
-			else
-				selecting = "option"
+	if not kb.isVisible() then
+		if selecting == "option" then
+			if aPressed or rightPressed then
+				sfx.switch:play()
+				-- do stuff here
+			elseif bPressed or leftPressed then
+				sfx.switch:play()
+				selecting = "map"
+			elseif upPressed then
+				optionSelection -= 1
+			elseif downPressed then
+				optionSelection += 1
 			end
-		elseif bPressed or leftPressed then
-			sfx.switch:play()
-			return "songSelect"
-		elseif upPressed then
-			mapSelection -= 1
-		elseif downPressed then
-			mapSelection += 1
+			
+			optionSelection += crankChange/90
+		elseif selecting == "map" then
+			if aPressed or rightPressed then
+				if mapSelectionRounded == #songMaps then
+					-- pull up keyboard to create map
+					kb.show()
+					kb.keyboardWillHideCallback = keyboardClosing
+					kb.textChangedCallback = keyboardChanged
+					oldText = songMaps[mapSelectionRounded]
+					songMaps[mapSelectionRounded] = ""
+				else
+					selecting = "option"
+				end
+				sfx.tap:play()
+			elseif bPressed or leftPressed then
+				sfx.low:play()
+				return "songSelect"
+			elseif upPressed then
+				mapSelection -= 1
+			elseif downPressed then
+				mapSelection += 1
+			end
+			
+			mapSelection += crankChange/90
 		end
-		
-		mapSelection += crankChange/90
 	end
 	
 	optionSelection = math.min(#mapOptions, math.max(optionSelection, 1))
@@ -118,11 +146,11 @@ function drawMapSongsSelect()
 	gfx.setLineWidth(2)
 	gfx.drawLine(133, 0, 133, screenHeight)
 	
-	drawScrollingList(songMaps, mapSelectionRounded, 12, 22, 110, 216, 3, fonts.orbeatsSans, selecting == "map", true)
+	drawScrollingList(songMaps, mapSelectionRounded, 12, 22, 110, 216, 3, fonts.orbeatsSans, selecting == "map" and not kb.isVisible(), true)
 	
 	gfx.setColor(gfx.kColorWhite)
 	gfx.setLineWidth(2)
 	gfx.drawLine(267, 0, 267, screenHeight)
 	
-	drawList(mapOptions, optionSelectionRounded, 136, 3, 110, 216, 3, fonts.orbeatsSans, selecting == "option", true)
+	drawList(mapOptions, optionSelectionRounded, 136, 3, 110, 216, 3, fonts.orbeatsSans, selecting == "option" and not kb.isVisible(), true)
 end
