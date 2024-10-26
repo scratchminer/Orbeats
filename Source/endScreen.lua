192ea6b7e8fa0b47ee8d9a23093b97bd831d3889
Nanobot567 <70984367+Nanobot567@users.noreply.github.com>
2024-10-26 12:26:12 -0500

    Merge Palette's changes into main

--- Source/endScreen.lua
+++ Source/endScreen.lua
@@ -1,4 +1,3 @@
-
 -- Define constants
 local pd <const> = playdate
 local gfx <const> = pd.graphics
@@ -27,7 +26,7 @@ ratingImage.D = gfx.image.new("sprites/ratingD")
 ratingImage.F = gfx.image.new("sprites/ratingF")
 local fullComboImage = gfx.image.new("sprites/fullCombo")
 local ratingImageWidth, ratingImageHeight = ratingImage.SS:getSize()
-local ratingY = (screenHeight-ratingImageHeight)/2
+local ratingY = (screenHeight - ratingImageHeight) / 2
 
 -- Animation variables
 local completedCurrentY = -45
@@ -44,7 +43,7 @@ local continueYTimer = tmr.new(0, continueCurrentY, continueCurrentY)
 local bgmTimer
 local tickerTimer = tmr.new(0, 0, 0)
 tickerTimer.repeats = true
-local tickerText = "To Menu:"..char.up.."/"..char.A.." --- Retry:"..char.down.."/"..char.B.." --- "
+local tickerText = "To Menu:" .. char.up .. "/" .. char.A .. " --- Retry:" .. char.down .. "/" .. char.B .. " --- "
 local tickerTextWidth = gfx.getTextSize(tickerText, fonts.orbeatsSans)
 
 local sheenTimer = tmr.new(0, 600, 600)
@@ -192,21 +191,20 @@ local function initEndScreen()
 end
 
 local function resetAnimationValues()
-	sheenTimer = replaceTimer(sheenTimer, 0, 600, 600)
-	completedCurrentY = -45
-	completedYTimer = replaceTimer(completedYTimer, 0, completedCurrentY, completedCurrentY)
-	statsCurrentX = -165
-	statsXTimer = replaceTimer(statsXTimer, 0, statsCurrentX, statsCurrentX)
-	ratingCurrentX = screenWidth+ratingImage.SS:getSize()
-	ratingXTimer = replaceTimer(ratingXTimer, 0, ratingCurrentX, ratingCurrentX)
-	continueCurrentY = screenHeight+25
-	continueYTimer = replaceTimer(continueYTimer, 0, continueCurrentY, continueCurrentY)
-	fadeOutBlack = 1
-	fadeOutWhite = 1
+  sheenTimer = replaceTimer(sheenTimer, 0, 600, 600)
+  completedCurrentY = -45
+  completedYTimer = replaceTimer(completedYTimer, 0, completedCurrentY, completedCurrentY)
+  statsCurrentX = -165
+  statsXTimer = replaceTimer(statsXTimer, 0, statsCurrentX, statsCurrentX)
+  ratingCurrentX = screenWidth + ratingImage.SS:getSize()
+  ratingXTimer = replaceTimer(ratingXTimer, 0, ratingCurrentX, ratingCurrentX)
+  continueCurrentY = screenHeight + 25
+  continueYTimer = replaceTimer(continueYTimer, 0, continueCurrentY, continueCurrentY)
+  fadeOutBlack = 1
+  fadeOutWhite = 1
 end
 
 function updateEndScreen()
-	
 	-- initialize the end screen
 	if not initialized then
 		initEndScreen()
@@ -256,93 +254,92 @@ function updateEndScreen()
 end
 
 function drawEndScreen()
-	
-	-- draw background sheen
-	local sheenX = sheenTimer.value
-	gfx.setColor(gfx.kColorWhite)
-	gfx.setDitherPattern(0.5)
-	gfx.setLineWidth(250)
-	gfx.drawLine(sheenX, screenHeight+50, sheenX+30, -50)
-	
-	-- draw completed text
-	completedCurrentY = completedYTimer.value
-	if failed then
-		gfx.drawText("Song Failed.", 5, completedCurrentY, fonts.odinRounded)
-	else
-		gfx.drawText("Song Completed!", 5, completedCurrentY, fonts.odinRounded)
-	end
-	
-	-- draw stats bubbles
-	local statsY = 55
-	gfx.setColor(gfx.kColorWhite)
-	statsCurrentX = statsXTimer.value
-	gfx.fillRoundRect(statsCurrentX, statsY, 165, 155, 3)
-	-- draw stats
-	gfx.setColor(gfx.kColorBlack)
-	gfx.drawText("Perfect Hits: "..perfectHits, statsCurrentX+5, statsY+5, fonts.orbeatsSans)
-	gfx.drawText("Hits: "..hitNotes, statsCurrentX+5, statsY+30, fonts.orbeatsSans)
-	gfx.drawText("Misses: "..missedNotes, statsCurrentX+5, statsY+55, fonts.orbeatsSans)
-	
-	gfx.drawText("Combo: "..largestCombo, statsCurrentX+5, statsY+80, fonts.orbeatsSans)
-	if songHiCombo < largestCombo then
-		gfx.drawText("New Best Combo!", statsCurrentX+5, statsY+100, fonts.orbeatsSmall)
-	else
-		gfx.drawText("Best Combo: "..songHiCombo, statsCurrentX+5, statsY+100, fonts.orbeatsSmall)
-	end
-	
-	gfx.drawText("Score: "..score, statsCurrentX+5, statsY+120, fonts.orbeatsSans)
-	if songHiScore < score then
-		gfx.drawText("New Best Score!", statsCurrentX+5, statsY+140, fonts.orbeatsSmall)
-	else
-		gfx.drawText("Best Score: "..songHiScore, statsCurrentX+5, statsY+140, fonts.orbeatsSmall)
-	end
-	
-	-- draw rating
-	ratingCurrentX = ratingXTimer.value
-	if songRating == "P" then
-		ratingImage.P:draw(ratingCurrentX, ratingY)
-	elseif songRating == "SS" then
-		ratingImage.SS:draw(ratingCurrentX, ratingY)
-	elseif songRating == "S" then
-		ratingImage.S:draw(ratingCurrentX, ratingY)
-	elseif songRating == "A" then
-		ratingImage.A:draw(ratingCurrentX, ratingY)
-	elseif songRating == "B" then
-		ratingImage.B:draw(ratingCurrentX, ratingY)
-	elseif songRating == "C" then
-		ratingImage.C:draw(ratingCurrentX, ratingY)
-	elseif songRating == "D" then
-		ratingImage.D:draw(ratingCurrentX, ratingY)
-	else
-		ratingImage.F:draw(ratingCurrentX, ratingY)
-	end
-	
-	-- draw full combo sprite if full combo
-	if fullCombo then
-		fullComboImage:draw(ratingCurrentX+33, ratingY+95)
-	end
-	
-	-- draw continue bar
-	continueCurrentY = continueYTimer.value
-	gfx.setColor(gfx.kColorWhite)
-	gfx.fillRect(0, continueCurrentY, screenWidth, 25)
-	-- draw continue text
-	local tickerX1 = tickerTimer.value
-	local tickerX2 = tickerTimer.value-tickerTextWidth
-	local tickerX3 = tickerTimer.value+tickerTextWidth
-	gfx.drawText(tickerText, tickerX1, continueCurrentY+5, fonts.orbeatsSans)
-	gfx.drawText(tickerText, tickerX2, continueCurrentY+5, fonts.orbeatsSans)
-	gfx.drawText(tickerText, tickerX3, continueCurrentY+5, fonts.orbeatsSans)
-	
-	-- draw fade out if fading out
-	if fadeOutWhite ~= 1 then
-		gfx.setColor(gfx.kColorWhite)
-		gfx.setDitherPattern(fadeOutWhite)
-		gfx.fillRect(0, 0, screenWidth, screenHeight)
-	end
-	if fadeOutBlack ~= 1 then
-		gfx.setColor(gfx.kColorBlack)
-		gfx.setDitherPattern(fadeOutBlack)
-		gfx.fillRect(0, 0, screenWidth, screenHeight)
-	end
+  -- draw background sheen
+  local sheenX = sheenTimer.value
+  gfx.setColor(gfx.kColorWhite)
+  gfx.setDitherPattern(0.5)
+  gfx.setLineWidth(250)
+  gfx.drawLine(sheenX, screenHeight + 50, sheenX + 30, -50)
+
+  -- draw completed text
+  completedCurrentY = completedYTimer.value
+  if failed then
+    gfx.drawText("Song Failed.", 5, completedCurrentY, fonts.odinRounded)
+  else
+    gfx.drawText("Song Completed!", 5, completedCurrentY, fonts.odinRounded)
+  end
+
+  -- draw stats bubbles
+  local statsY = 55
+  gfx.setColor(gfx.kColorWhite)
+  statsCurrentX = statsXTimer.value
+  gfx.fillRoundRect(statsCurrentX, statsY, 165, 155, 3)
+  -- draw stats
+  gfx.setColor(gfx.kColorBlack)
+  gfx.drawText("Perfect Hits: " .. perfectHits, statsCurrentX + 5, statsY + 5, fonts.orbeatsSans)
+  gfx.drawText("Hits: " .. hitNotes, statsCurrentX + 5, statsY + 30, fonts.orbeatsSans)
+  gfx.drawText("Misses: " .. missedNotes, statsCurrentX + 5, statsY + 55, fonts.orbeatsSans)
+
+  gfx.drawText("Combo: " .. largestCombo, statsCurrentX + 5, statsY + 80, fonts.orbeatsSans)
+  if songHiCombo < largestCombo then
+    gfx.drawText("New Best Combo!", statsCurrentX + 5, statsY + 100, fonts.orbeatsSmall)
+  else
+    gfx.drawText("Best Combo: " .. songHiCombo, statsCurrentX + 5, statsY + 100, fonts.orbeatsSmall)
+  end
+
+  gfx.drawText("Score: " .. score, statsCurrentX + 5, statsY + 120, fonts.orbeatsSans)
+  if songHiScore < score then
+    gfx.drawText("New Best Score!", statsCurrentX + 5, statsY + 140, fonts.orbeatsSmall)
+  else
+    gfx.drawText("Best Score: " .. songHiScore, statsCurrentX + 5, statsY + 140, fonts.orbeatsSmall)
+  end
+
+  -- draw rating
+  ratingCurrentX = ratingXTimer.value
+  if songRating == "P" then
+    ratingImage.P:draw(ratingCurrentX, ratingY)
+  elseif songRating == "SS" then
+    ratingImage.SS:draw(ratingCurrentX, ratingY)
+  elseif songRating == "S" then
+    ratingImage.S:draw(ratingCurrentX, ratingY)
+  elseif songRating == "A" then
+    ratingImage.A:draw(ratingCurrentX, ratingY)
+  elseif songRating == "B" then
+    ratingImage.B:draw(ratingCurrentX, ratingY)
+  elseif songRating == "C" then
+    ratingImage.C:draw(ratingCurrentX, ratingY)
+  elseif songRating == "D" then
+    ratingImage.D:draw(ratingCurrentX, ratingY)
+  else
+    ratingImage.F:draw(ratingCurrentX, ratingY)
+  end
+
+  -- draw full combo sprite if full combo
+  if fullCombo then
+    fullComboImage:draw(ratingCurrentX + 33, ratingY + 95)
+  end
+
+  -- draw continue bar
+  continueCurrentY = continueYTimer.value
+  gfx.setColor(gfx.kColorWhite)
+  gfx.fillRect(0, continueCurrentY, screenWidth, 25)
+  -- draw continue text
+  local tickerX1 = tickerTimer.value
+  local tickerX2 = tickerTimer.value - tickerTextWidth
+  local tickerX3 = tickerTimer.value + tickerTextWidth
+  gfx.drawText(tickerText, tickerX1, continueCurrentY + 5, fonts.orbeatsSans)
+  gfx.drawText(tickerText, tickerX2, continueCurrentY + 5, fonts.orbeatsSans)
+  gfx.drawText(tickerText, tickerX3, continueCurrentY + 5, fonts.orbeatsSans)
+
+  -- draw fade out if fading out
+  if fadeOutWhite ~= 1 then
+    gfx.setColor(gfx.kColorWhite)
+    gfx.setDitherPattern(fadeOutWhite)
+    gfx.fillRect(0, 0, screenWidth, screenHeight)
+  end
+  if fadeOutBlack ~= 1 then
+    gfx.setColor(gfx.kColorBlack)
+    gfx.setDitherPattern(fadeOutBlack)
+    gfx.fillRect(0, 0, screenWidth, screenHeight)
+  end
 end
